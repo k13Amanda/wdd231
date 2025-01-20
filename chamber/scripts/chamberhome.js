e8607b1f817211854594b8f996321ccd57b2c6d4
k13amanda <u232377037@byui.edu>
2025-01-20 11:58:02 -0700

    home page weather

--- chamber/scripts/chamberhome.js
+++ chamber/scripts/chamberhome.js
@@ -71,5 +71,42 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
 
+// 02f154a08ba74fabee63f6aa1cde9c7e
 
+const myTown = document.querySelector('#town');
+const myDesc = document.querySelector('#desc');
+const myTemp = document.querySelector('#temp');
+const myGraphic = document.querySelector('#graphic');
 
+const myKey = "02f154a08ba74fabee63f6aa1cde9c7e";
+const myLat = "27.049954761550023";
+const myLong ="-82.23954575506782";
+
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+
+async function apiFetch() {
+    try{
+        const response = await fetch(myURL); // Corrected 'fatch' to 'fetch'
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {  
+            throw Error(await response.text());
+        }
+    } catch (error){
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    console.log('hello');
+    myTown.innerHTML = data.name;
+    myDesc.innerHTML = data.weather[0].description;
+    myTemp.innerHTML = `${data.main.temp}&deg;F`; // Fixed template literal
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    myGraphic.setAttribute('src', iconsrc);
+    myGraphic.setAttribute('alt', data.weather[0].description);
+}
+
+apiFetch();
