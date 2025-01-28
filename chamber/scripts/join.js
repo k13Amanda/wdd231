38d7ca51e5a91359de596e370450b85db3be11d6
k13amanda <u232377037@byui.edu>
2025-01-28 12:08:06 -0700

    join page

---
+++ chamber/scripts/join.js
@@ -0,0 +1,55 @@
+// currentyear
+const yearSpan = document.querySelector("#currentyear");
+const today = new Date();
+const currentYear = today.getFullYear();
+yearSpan.textContent = currentYear;
+
+
+// lastModified
+const dateSpan = document.querySelector("#lastModified");
+const now = new Date();
+const lastModified = document.lastModified
+const formattedDate = `${String(now.getMonth() + 1).padStart(2, '0')}/${String(now.getDate()).padStart(2, '0')}/${now.getFullYear()}`;
+const formattedTime = `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
+const formattedDateTime = `${formattedDate} ${formattedTime}`;
+
+dateSpan.textContent = `Last Modified: ${formattedDateTime}`;
+
+
+// hamburger button
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
+
+
+
+
+
+document.querySelectorAll('.member-card a').forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.style.display = 'block';
+    });
+  });
+  
+  // Close modals when clicking on the close button
+  document.querySelectorAll('.modal-content .close').forEach(button => {
+    button.addEventListener('click', function() {
+      const modal = this.closest('.modal');
+      modal.style.display = 'none';
+    });
+  });
+  
+  // Close modals when clicking outside of them
+  window.addEventListener('click', function(event) {
+    if (event.target.classList.contains('modal')) {
+      event.target.style.display = 'none';
+    }
+  });
+  
