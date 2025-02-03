13a2318a909d059b14b49ca2285f2cab607475c5
k13amanda <u232377037@byui.edu>
2025-02-02 21:08:47 -0700

    new

---
+++ project/scripts/bluga.js
@@ -0,0 +1,27 @@
+// currentyear
+const yearSpan = document.querySelector("#currentyear");
+const today = new Date();
+const currentYear = today.getFullYear();
+yearSpan.textContent = currentYear;
+
+
+// lastModified
+const dateSpan = document.querySelector("#lastModified");
+const now = new Date();
+const lastModified = document.lastModified
+const formattedDate = `${String(now.getMonth() + 1).padStart(2, '0')}/${String(now.getDate()).padStart(2, '0')}/${now.getFullYear()}`;
+const formattedTime = `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
+const formattedDateTime = `${formattedDate} ${formattedTime}`;
+
+dateSpan.textContent = `Last Modified: ${formattedDateTime}`;
+
+
+// hamburger button
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
